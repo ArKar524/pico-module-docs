b030de786cb54f91840307dd0054158aeed6ef3c
Ar Kar Moe <moearkar963@gmail.com>
2025-07-29 15:00:56 +0630

    modified next config #2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // ✅ export as static HTML (Next.js 13+)
-  trailingSlash: true, // ✅ optional: makes /docs/ work cleanly
 };
 
 export default nextConfig;
